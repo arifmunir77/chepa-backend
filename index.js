274826dbc44b7206bdaa9a3ffd0651bb1d516ddf
arifmunir77 <129254701+arifmunir77@users.noreply.github.com>
2023-08-05 12:09:31 +0500

    updates

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 /*********** import starts ***********/
 const http = require("http");
-
+const socketio = require("socket.io");
+const { channel } = require("./app/socket.io");
 require("dotenv").config();
 ("use strict");
 const koa = require("koa");
@@ -10,15 +11,30 @@ const { router } = require("./app/routes");
 const { connectDB } = require("./app/utils/db");
 const { env } = require("./environment");
 const logger = require("./logger");
+const swagger = require("swagger2");
+const { ui, validate } = require("swagger2-koa");
+ 
+const swaggerDocument = swagger.loadDocumentSync("./App/swagger/api.yaml");
 
 const app = new koa();
 
 connectDB();
 app.use(cors());
 app.use(bodyParser());
-
+app.use(ui(swaggerDocument, "/api_docs"));
 app.use(router.routes()).use(router.allowedMethods());
+
+
 
 const server = app.listen(env.PORT, () =>
   logger.info(`Server has started. http://localhost:${env.PORT}`)
 );
+
+const io = socketio(server, {
+  cors: {
+    origin: env.FRONTEND_BASE_URL,
+    methods: ["GET", "POST"],
+  },
+});
+channel(io);
+exports.io = io;
