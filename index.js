de42b9d7a17701905a82e4c4089cdfae70acf685
arifmunir77 <129254701+arifmunir77@users.noreply.github.com>
2023-08-05 11:23:24 +0500

    remove socket io

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /*********** import starts ***********/
 const http = require("http");
-const socketio = require("socket.io");
+ 
 const { channel } = require("./app/socket.io");
 require("dotenv").config();
 ("use strict");
@@ -29,12 +29,4 @@ app.use(router.routes()).use(router.allowedMethods());
 const server = app.listen(env.PORT, () =>
   logger.info(`Server has started. http://localhost:${env.PORT}`)
 );
-
-const io = socketio(server, {
-  cors: {
-    origin: env.FRONTEND_BASE_URL,
-    methods: ["GET", "POST"],
-  },
-});
-channel(io);
-exports.io = io;
+ 
